6f3fde598c40921687f03bc14b843e3972109c12
Sanford Staab <sanfords@gmail.com>
2024-11-27 14:18:26 -0800

    minor improvements

--- table/tictactoe/assignment4.js
+++ table/tictactoe/assignment4.js
@@ -44,7 +44,7 @@ async function as_eh_onCellClick(event) {
   if (g.fGameOver) {
     return; // no clicks allow till game is being played
   }
-  const eCell = ge(event.target.id);
+  const eCell = event.target;
   if (eCell.innerText.trim() == '') { // is blank square?
     eCell.innerText = 'X'; // make our move
     await as_delay(1); // let UI catch up
@@ -56,7 +56,7 @@ async function as_eh_onCellClick(event) {
       g.fGameOver = true;
       waitAndClearBoard();
     } else {
-      setTimeout(makeAMove, 1); // lets UI catch up
+      makeAMove();
     }
   }
 }
@@ -169,9 +169,8 @@ function checkForWinState(nMyState) {
     const eCell = ge(cellId);
     const sCell = eCell.innerHTML.trim();
     // console.log(`${cellId}=${sCell}`);
-    const testBit = 2 ** nBit;
     if (sCell == sSide1 || sCell == sSide2) {
-      nXCells |= testBit;
+      nXCells |= 2 ** nBit;
     }
   }
   // dumpBits(nXCells, `getBoardState(${sSide1}, ${sSide2})=> 0b`)
@@ -305,7 +304,7 @@ function makeRndCellOrderArray() {
   for (let i = 0; i < 0; i++) {
     console.assert(aCellsInOrder[i] >= 0 && aCellsInOrder[i] <= 8);
   }
-  console.assert(!aCellsInOrder.includes(-1));
+  console.assert(!aCellsInOrder.includes(nEmptyCell));
 
   return aCellsInOrder;
 }
@@ -403,7 +402,7 @@ function checkFor2InARowState(
   //   nPlayerState=${nPlayerState.toString(2)}, 
   //   nThisBit=${nThisBit}, 2 in a row: ${fWillHave2InARow}`);
 
-  return !!fWillHave2InARow;
+  return fWillHave2InARow;
 }
 
 function dumpBits(nBits, prefix='') {
