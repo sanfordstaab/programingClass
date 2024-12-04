34ffcb175ae245cba904241281cbd499e7ce39be
Avery <ipadacj@gmail.com>
2024-12-04 14:13:22 -0800

    Added code for adding and subtracting vectors

--- matrix/avery/matrix.js
+++ matrix/avery/matrix.js
@@ -3,5 +3,38 @@ function ge(id) {
 }
 
 function eh_processInput() {
-    const eval = ge('textInput');
+    const elOut = ge('spnOutput');
+    let sValue = ge('txtaInput').value;
+    sValue = '()=>{' + sValue + 'return [ A, B ,operation ]}';
+    let calc = eval(sValue)();
+    console.log(calc);
+
+    const op = calc[2];
+    let ans = [];
+    if (op == '+') {
+        ans = addVectors(calc[0], calc[1]);
+    } else if (op == '-') {
+        ans = subVectors(calc[0], calc[1]);
+    }
+    console.log(ans);
+    ge('spnOutput').innerText = JSON.stringify(ans);
+
+}
+
+function addVectors ( A, B ) {
+    if (A.lenght != B.lenght) {
+        throw new Error('Both arrays must be of the same lenght!');
+    }
+    const result = [];
+    for (let i = 0; i < A.length; i++){
+        result [i] = A[i] + B[i];
+    }
+    return result;
+}
+function subVectors ( A, B ) {
+    const result = [];
+    for (let i = 0; i < A.length; i++){
+        result [i] = A[i] - B[i];
+    }
+    return result;
 }
