317ad5a3665b91434ce3cb90f39b24b07732bf0e
Avery <ipadacj@gmail.com>
2024-12-05 09:21:34 -0800

    Need Help
    The error messages will not show up.

--- matrix/avery/matrix.js
+++ matrix/avery/matrix.js
@@ -3,38 +3,43 @@ function ge(id) {
 }
 
 function eh_processInput() {
-    const elOut = ge('spnOutput');
-    let sValue = ge('txtaInput').value;
-    sValue = '()=>{' + sValue + 'return [ A, B ,operation ]}';
-    let calc = eval(sValue)();
-    console.log(calc);
-
-    const op = calc[2];
-    let ans = [];
-    if (op == '+') {
-        ans = addVectors(calc[0], calc[1]);
-    } else if (op == '-') {
-        ans = subVectors(calc[0], calc[1]);
+    try {
+        const elOut = ge('spnOutput');
+            let sValue = ge('txtaInput').value;
+            sValue = '()=>{' + sValue + 'return [ A, B ,operation ]}';
+            let calc = eval(sValue)();
+            console.log(calc);
+    
+            const op = calc[2];
+            let ans = [];
+            if (op == '+') {
+                ans = addVectors(calc[0], calc[1]);
+            } else if (op == '-') {
+                ans = subVectors(calc[0], calc[1]);
+            }
+            console.log(ans);
+            elOut.innerText = JSON.stringify(ans);
+            elOut.className = 'black';
+    } catch(error) {
+        elOut.innerText = error.message;
+        elOut.className = 'red';
+      }
     }
-    console.log(ans);
-    ge('spnOutput').innerText = JSON.stringify(ans);
 
-}
-
-function addVectors ( A, B ) {
+function addVectors(A, B) {
     if (A.lenght != B.lenght) {
         throw new Error('Both arrays must be of the same lenght!');
     }
     const result = [];
-    for (let i = 0; i < A.length; i++){
-        result [i] = A[i] + B[i];
+    for (let i = 0; i < A.length; i++) {
+        result[i] = A[i] + B[i];
     }
     return result;
 }
-function subVectors ( A, B ) {
+function subVectors(A, B) {
     const result = [];
-    for (let i = 0; i < A.length; i++){
-        result [i] = A[i] - B[i];
+    for (let i = 0; i < A.length; i++) {
+        result[i] = A[i] - B[i];
     }
     return result;
 }
