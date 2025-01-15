5a47dcbef9d5f3b4cb8226e5f566f44e403fdb35
Sanford Staab <sanfords@gmail.com>
2025-01-15 14:43:16 -0800

    add for and switch.js

---
+++ increment/switch.js
@@ -0,0 +1,14 @@
+switch (value) {
+    case 1:
+        // code
+        break;
+
+    case 2:
+    case 3:
+        // code for 2 & 3
+        break;
+
+    defaul:
+        // codde if nothing was hit
+        break;
+}
